4dba0f66b2f5d220892eabbd065f1bf8478da8e4
David Parro Plaza <42015059+David-Parro-P@users.noreply.github.com>
2023-04-16 22:57:44 +0200

    ejercicio_2

---
+++ ejercicio_nums.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 16 22:04:21 2023
+
+@author: david
+"""
+
+from paho.mqtt.client import Client
+import sys
+
+def on_message(client, userdata, msg):
+    print("Mensaje:", msg.topic, "El numero es: ", msg.payload)
+    value = float(msg.payload.decode("utf-8"))
+    messages.append(value)
+    if value.is_integer():
+        if value%2 == 0:
+            print("Par")
+        else:
+            print("Impar")
+    else:
+        print("Real")
+    
+def main(hostname):
+    client = Client()
+    client.enable_logger()
+    client.on_message = on_message
+    client.connect("simba.fdi.ucm.es")
+    client.subscribe('numbers/#')
+    client.loop_forever()
+
+if __name__ == '__main__':
+    hostname = 'simba.fdi.ucm.es'
+    if len(sys.argv)>1:
+        hostname = sys.argv[1]
+    messages = []
+    main(hostname)
